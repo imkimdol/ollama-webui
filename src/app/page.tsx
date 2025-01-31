493834f328aef9b25fe83cb8b2bee7949553c778
imkimdol <kimdol@proton.me>
2025-01-31 10:10:28 -0800

    Make model choice into a dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,11 +52,13 @@ export default function Home() {
   return (
     <div>
       <p>API is {apiIsOnline ? 'online' : 'offline'}</p>
-      {models.map(m => {
-        return <p key={m.name}>{m.name}</p>
-      })}
+      
       <p>{response}</p>
-      <input type="text" value={model} onChange={(e) => setCurrentModel(e.target.value)}/>
+      <select value={model} onChange={(e) => setCurrentModel(e.target.value)}>
+        {models.map(m => {
+          return <option value={m.name} key={m.name}>{m.name}</option>
+        })}
+      </select>
       <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder={'Prompt goes here'}/>
       <button onClick={() => {onSend()}}>Send Request</button>
     </div>
