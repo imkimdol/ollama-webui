bde9764417e60b5d401ce2a439e2d02347c59378
imkimdol <kimdol@proton.me>
2025-01-31 10:12:57 -0800

    Move page to "/generate"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,11 @@
 'use client'
 
-import { useState, useEffect } from 'react';
-import ollama, { ModelResponse } from 'ollama/browser';
+import Link from 'next/link';
 
 export default function Home() {
-  const [apiIsOnline, setApiIsOnline] = useState<boolean>(false);
-  const [models, setModels] = useState<ModelResponse[]>([]);
-
-  const [model, setCurrentModel] = useState<string>('llama3.2');
-  const [prompt, setPrompt] = useState<string>('');
-  const [response, setResponse] = useState<string>('Prompt response goes here!');
-
-  useEffect(() => {
-    checkModels();
-    setInterval(() => checkModels(), 10000);
-  }, []);
-
-
-  const checkModels = async () => {
-    try {
-      const response = await ollama.list();
-      const models = response.models;
-      setApiIsOnline(true);
-      setModels(models);
-    } catch {
-      setApiIsOnline(false);
-      setModels([]);
-    }
-  };
-  
-  const onSend = async () => {
-    setResponse('Requesting...');
-    try {
-      const responseIterator = await ollama.generate({
-        model: model,
-        prompt: prompt,
-        stream: true
-      });
-  
-      let partialText: string = '';
-      for await (const partialResponse of responseIterator) {
-        partialText += partialResponse.response;
-        setResponse(partialText);
-      }
-    } catch {
-      setResponse('Prompt failed');
-      checkModels();
-    }
-  };
-
   return (
     <div>
-      <p>API is {apiIsOnline ? 'online' : 'offline'}</p>
-      
-      <p>{response}</p>
-      <select value={model} onChange={(e) => setCurrentModel(e.target.value)}>
-        {models.map(m => {
-          return <option value={m.name} key={m.name}>{m.name}</option>
-        })}
-      </select>
-      <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder={'Prompt goes here'}/>
-      <button onClick={() => {onSend()}}>Send Request</button>
+      <Link href="/generate">Go to generate</Link>
     </div>
   );
 };
